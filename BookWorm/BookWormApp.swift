13abdf33da1e3ff565adc16cbc167bdf967f7e21
Weerawut <weerawut@live.com>
2024-12-14 08:54:27 +0700

    Initial Commit

---
+++ BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by Weerawut Chaiyasomboon on 14/12/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
